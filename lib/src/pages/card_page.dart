fdb66fa03129cd4dea5410b7a9e31fa2a4ebf3f4
vidal1101 <rodrigovidal278@gmail.com>
2021-01-29 22:16:01 -0600

    Avanzado las cards y personalizacion

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,13 +9,100 @@ class Card_Page extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10),
-        children: <Widget>[_CardTipo1()],
+        children: <Widget>[
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _CardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+        ],
       ),
     );
   }
 
+
+
   Widget _CardTipo1() {
     return Card(
+      elevation: 17.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(45.0),
+      ),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -24,10 +111,57 @@ class Card_Page extends StatelessWidget {
               color: Colors.brown,
             ),
             title: Text("FOTO"),
-            subtitle: Text("no hay imagen para mostrar.."),
+            subtitle: Text(
+                "Esta es la descripción de la tarjeta para mostrar en la App."),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(onPressed: () {}, child: Text('CANCELAR')),
+              FlatButton(onPressed: () {}, child: Text('OK'))
+            ],
           )
         ],
       ),
     );
   }
 }
+
+Widget _cardTipo2() {
+  final cards = Container(
+    child: Column(
+      children: <Widget>[
+        FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(seconds: 4),
+          image: NetworkImage(
+              'https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
+        ),
+        Container(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Nueva Zelanda'),
+        )
+      ],
+    ),
+  );
+
+  return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 12.5,
+              spreadRadius: 2.3,
+              offset: Offset(3.0, 10.0),
+            )
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(45.0),
+        child: cards,
+      ));
+
+}
+
+
