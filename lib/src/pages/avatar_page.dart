c5d83640fc0411346cdfbd97338ff65c75625eab
vidal1101 <rodrigovidal278@gmail.com>
2021-02-04 21:59:46 -0600

    se añadio los input y animacion

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          //avatar
+          Container(
+            padding: EdgeInsets.all(3.0),
+            margin: EdgeInsets.only(right: 5.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/avatarmeli.jpg'),
+              radius: 25.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 15.0),
+            child: CircleAvatar(
+              child: Text('ML'),
+              backgroundColor: Colors.purpleAccent,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            FadeInImage(
+              image: AssetImage('assets/meli.jpg'),
+              fadeInDuration: Duration(seconds: 5),
+              placeholder: AssetImage('assets/jar-loading.gif'),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('Te Amo princesa'),
+              
+              
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/src/routes/routes_page.dart
+++ lib/src/routes/routes_page.dart
@@ -4,15 +4,17 @@ import 'package:componetesflutter/src/pages/alerts_page.dart';
 import 'package:componetesflutter/src/pages/avatar_page.dart';
 import 'package:componetesflutter/src/pages/home_page.dart';
 import 'package:componetesflutter/src/pages/card_page.dart';
-
+import 'package:componetesflutter/src/pages/animated_container.dart';
+import 'package:componetesflutter/src/pages/inputs_page.dart';
 
 Map<String, WidgetBuilder> getApliacationRoutes() {
-
   return <String, WidgetBuilder>{
-    '/'       : (BuildContext context) => Home_Page(),
-    'alert'   : (BuildContext context) => AlertsPage(),
-    'avatar'  : (BuildContext context) => AvatarPage(),
-    'card'    : (BuildContext context)=> Card_Page(),
-  };
+    '/'                 : (BuildContext context) => Home_Page(),
+    'alert'             : (BuildContext context) => AlertsPage(),
+    'avatar'            : (BuildContext context) => AvatarPage(),
+    'card'              : (BuildContext context) => Card_Page(),
+    'animatedContainer' : (BuildContext context) => Animated_Container(),
+    'inputs'            : (BuildContext context) => Inputs_Page(),
 
+  };
 }
